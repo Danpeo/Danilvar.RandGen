0f64316b9dc6b104cf5f42362702434d4887224c
Danpeo <danilvar4@gmail.com>
2024-02-09 18:01:21 +0400

    Some refactoring

--- DVar.RandCreds/RandGen.fs
+++ DVar.RandCreds/RandGen.fs
@@ -14,6 +14,11 @@ let private noSpecialChars = lowerChars + upperChars + numericChars
 let private getRandomChar (charSet: string) =
   charSet[RandomNumberGenerator.GetInt32(charSet.Length)]
 
+let private getLength (length: int, minLength: int, maxLength: int) : int =
+  if length <= minLength then length
+  else if length >= maxLength then maxLength
+  else length
+
 let Password (options: PasswordOptions) : string =
   let getPasswordChar i =
     match i with
@@ -22,10 +27,9 @@ let Password (options: PasswordOptions) : string =
     | 2 when options.NonAlpha -> specialChars
     | _ -> allChars
 
-  let length = if options.Length > 5 then options.Length else 6;
-  
-  let password =
-    Array.init length (fun i -> getRandomChar (getPasswordChar i))
+  let length = getLength (options.Length, 5, 100)
+
+  let password = Array.init length (fun i -> getRandomChar (getPasswordChar i))
 
   let shufflePassword (password: char array) =
     let random = Random()
@@ -34,27 +38,34 @@ let Password (options: PasswordOptions) : string =
   let shuffledPassword = new string (password |> shufflePassword)
   shuffledPassword
 
-let PasswordDefault = Password {Length = 6; Lowercase = true; Uppercase = true; NonAlpha = true }
+let PasswordDefault =
+  Password
+    { Length = 6
+      Lowercase = true
+      Uppercase = true
+      NonAlpha = true }
 
 let Username (options: UsernameOptions) : string =
-  let postfixLength = if options.PostfixLength > 5 then options.PostfixLength else 6
-  
-  let genPostfix =
-    Array.init postfixLength (fun _ -> getRandomChar noSpecialChars)
+  let postfixLength = getLength (options.PostfixLength, 5, 100)
+
+  let genPostfix = Array.init postfixLength (fun _ -> getRandomChar noSpecialChars)
 
   options.Prefix + new string (genPostfix)
 
 let UsernameDefault = Username { Prefix = "user"; PostfixLength = 6 }
 
 let Email (options: EmailOptions) : string =
-  let charLength = if options.RandomCharLength > 5 then options.RandomCharLength else 6
-  
+  let charLength = getLength (options.RandomCharLength, 5, 100)
+
   let postfix =
     options.Domains[RandomNumberGenerator.GetInt32(options.Domains.Length)]
 
-  let genMiddleChars =
-    Array.init charLength (fun _ -> getRandomChar noSpecialChars)
+  let genMiddleChars = Array.init charLength (fun _ -> getRandomChar noSpecialChars)
 
   options.Prefix + new string (genMiddleChars) + postfix
-  
-let EmailDefault = Email { Domains = [|"@mail.com"|]; Prefix = "user"; RandomCharLength = 6 }
+
+let EmailDefault =
+  Email
+    { Domains = [| "@mail.com" |]
+      Prefix = "user"
+      RandomCharLength = 6 }
